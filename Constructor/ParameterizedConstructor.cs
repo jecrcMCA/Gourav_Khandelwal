53eccad9bf7bb0f322f8cbf0b10be88432fab1b8
Gourav_Khandelwal <73248169+Gouravkhandelwal@users.noreply.github.com>
2020-11-01 22:21:08 +0530

    Add files via upload

---
+++ Constructor/ParameterizedConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ParameterizedConstructor
+{
+    class parCon
+    {
+
+        
+        String name;
+        int id;
+        parCon(String name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+ 
+        public static void Main()
+        {
+
+            parCon p1 = new parCon("Gaurav", 94);
+            Console.WriteLine("Name = " + p1.name +
+                             " and Id = " + p1.id);
+        }
+    }
+}
