fbeeefc22056f75aca13e4cd66734af9824f38d3
Gourav_Khandelwal <73248169+Gouravkhandelwal@users.noreply.github.com>
2020-11-01 22:52:50 +0530

    Add files via upload

---
+++ access_modifiers/All_accessModifiers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Access_modifiers
+{
+    class Car
+    {
+        public string Pub_model = "Public BMW";
+        private string Piv_model = "Private BMW";
+        protected string pro_Model = "Public BMW";
+        public void print_private_model()
+        {
+            Console.WriteLine(Piv_model);
+        }  
+    
+    
+    }
+
+  
+
+    class All_accessModifiers
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.Pub_model);
+            myObj.print_private_model();
+            //Console.WriteLine(myObj.Piv_model);
+            //Console.WriteLine(myObj.pro_model);
+
+        }
+    }
+}
