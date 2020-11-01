a535d21c8f350ea216a4b3d01bec8825a9571249
Gourav_Khandelwal <73248169+Gouravkhandelwal@users.noreply.github.com>
2020-11-01 22:30:57 +0530

    Add files via upload

---
+++ Constructor/staticConstractor.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+namespace staticConstructor
+{
+
+    class staticCon
+    {
+
+        static staticCon()
+        {
+
+            Console.WriteLine("Static Constructor");
+        }
+
+
+        public staticCon(int i)
+        {
+            Console.WriteLine("Instance Constructor " + i);
+        }
+
+
+        public string detail(string name, int id)
+        {
+            return "Name:" + name + " id:" + id;
+        }
+
+
+        public static void Main()
+        {
+
+            staticCon obj = new staticCon(1);
+
+            Console.WriteLine(obj.detail("Gaurav", 1));
+            staticCon obj1 = new staticCon(2);
+
+            Console.WriteLine(obj1.detail("Gourav", 2));
+        }
+    }
+}
