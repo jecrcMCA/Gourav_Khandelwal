4f6e421c3287742d75ef367901daf0fff0d75cbe
Gourav_Khandelwal <73248169+Gouravkhandelwal@users.noreply.github.com>
2020-11-01 22:06:18 +0530

    Add files via upload

---
+++ Constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Def_Constructor
+{
+   
+
+    class Program
+    {
+        int num;
+        string name;
+
+        Program()
+        {
+            Console.WriteLine("Hii i am Constructor Called");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+
+            Console.WriteLine(p1.name);
+            Console.WriteLine(p1.num);
+        }
+    }
+}
